1527e94d57cd5b4bf9a00bcc6b6702de3235ff7b
Alain De Long <leholong007@gmail.com>
2025-11-13 10:54:13 +0700

    chore: Add config and data

---
+++ config.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+import torch
+
+# Path Configuration
+DATA_PATH = Path(r"data\IWSLT-15-en-vi")
+
+# TOKENIZER_NAME = ""
+TOKENIZER_NAME = "iwslt_en-vi_tokenizer_32k"
+TOKENIZER_PATH = Path(r"artifacts\tokenizers") / TOKENIZER_NAME
+
+# MODEL_NAME = ""
+MODEL_NAME = "transformer_en_vi_iwslt_1"
+MODEL_SAVE_PATH = Path(r"artifacts\models") / MODEL_NAME
+
+CACHE_DIR = ""
+
+
+# Hardware & Data Config
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+NUM_WORKERS: int = 4
+
+NUM_SAMPLE_TO_USE: int = 1_000_000
+
+BATCH_SIZE: int = 32
+
+
+# Tokenizer Constants
+PAD_TOKEN_ID: int = 0
+SOS_TOKEN_ID: int = 2
+EOS_TOKEN_ID: int = 3
+
+
+# Model Hyperparameters
+D_MODEL: int = 256  # (Dimension of model)
+N_LAYERS: int = 6  # (N=6 in paper)
+N_HEADS: int = 8  # (h=8 in paper)
+D_FF: int = 1024  # (d_ff = 4 * d_model = 1024)
+DROPOUT: float = 0.1  # (Dropout = 0.1 in paper)
+MAX_SEQ_LEN: int = 150  # (Max length for Positional Encoding)
+
+
+if __name__ == "__main__":
+    print(f"Using device: {DEVICE}")
